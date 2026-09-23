413e8ff491cb27559cefb286ebe91fa53a9a9f34
ccoueffe <ccoueffe@cisco.com>
2026-09-23 18:59:47 +0200

    perf: use nac-yaml 2.0.0 safe loader for model load
    Load the data model with typ='safe' (native dict/list, C-accelerated with ruamel.yaml.clib) instead of the round-trip loader. Pin nac-yaml==2.0.0 and nac-validate==2.0.0. Validation output unchanged; faster load on large fabrics.

--- plugins/action/common/nac_dc_validate.py
+++ plugins/action/common/nac_dc_validate.py
@@ -106,7 +106,7 @@ class ActionModule(ActionBase):
         if rules and task_vars['role_path'] in rules:
             # Load in-memory data model using iac-validate
             # Perform the load in this if block to avoid loading the data model multiple times when custom enhanced rules are provided
-            results['data'] = load_yaml_files([mdata])
+            results['data'] = load_yaml_files([mdata], typ='safe')
             data_model_loaded = results['data']
 
             # Introduce common directory to the rules list by default once vrf and network rules are updated
@@ -180,7 +180,7 @@ class ActionModule(ActionBase):
                     syntax_validated = True
                 if rules_item:
                     if data_model_loaded is None:
-                        data_model_loaded = load_yaml_files([mdata])
+                        data_model_loaded = load_yaml_files([mdata], typ='safe')
                         results['data'] = data_model_loaded
                     validator.data = data_model_loaded
                     validator.validate_semantics([mdata])
